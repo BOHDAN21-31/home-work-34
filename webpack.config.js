2e10d934c984a830e02f807bbb5fab4c29dc6045
Bohdan21-31 <bohdan.ivanii@gmail.com>
2025-08-06 23:48:10 +0300

    complite

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
+        assetModuleFilename: 'assets/[hash][ext][query]'
     },
+    plugins: [
+        new HTMLWebpackPlugin({ template: 'index.html' }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif|webp)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
 };
